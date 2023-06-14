e3a449eab02e8804e75792b88d568aadcbb4d461
rolaalfayomi <you@example.comrola.alfayomi1@gmail.com>
2023-06-14 16:03:28 +0300

    ee4

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let x =parseInt( prompt("please enter x") );
+let y=7;
+document.getElementById("mydiv").innerHTML=x*y;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+window.alert("Hello");
+
+let z = 1.0;
+let u="adf";
+document.write(u)
+document.write("<div><h1>BBBBBB</h1></div>");
+document.write(x === y);
+document.write("<br>");
+document.write(x === z);
+document.write("<br>");
+document.write(Number.EPSILON);
+console.log("Hello My Dears");
